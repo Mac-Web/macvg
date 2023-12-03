4efa3e37fef87a3665649677835ed71290b2d7da
Tony Hsu <mac.web.company@gmail.com>
2023-12-03 10:23:23 -0500

    Fortnite added!!!

--- favorites.js
+++ favorites.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const favoriteList = document.getElementById("favoriteList");
   const games = document.getElementById("list");
+  const fortnite = document.getElementById("list2");
   let items = games.getElementsByTagName("a");
+  let fortnites = fortnite.getElementsByTagName("a");
   let thing = Array.from(items);
+  let thing2 = Array.from(fortnites);
   let favorites = localStorage.favorites;
   let list = favorites.split(",");
   let clonedElements = [];
@@ -17,6 +20,14 @@ document.addEventListener("DOMContentLoaded", function () {
         const clonedGame = game.cloneNode(true);
         clonedElements.push(clonedGame);
         game.style.display = "flex";
+      }
+    });
+    thing2.forEach((game2) => {
+      let item2 = game2.getAttribute("id");
+      if (item2 === one) {
+        const clonedGame2 = game2.cloneNode(true);
+        clonedElements.push(clonedGame2);
+        game2.style.display = "flex";
       }
     });
   });

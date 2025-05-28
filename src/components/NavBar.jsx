4503e4fb7bcd43d121f87e2eda893b57e6a28149
tonymac129 <tonyforself@gmail.com>
2025-05-27 22:11:07 -0400

    Added nav search

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,52 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { logo, mode, github } from "../assets/assets";
+import { logo, mode, close, github, gamesData } from "../assets/assets";
+import GameCard from "./GameCard";
 
 function NavBar() {
+  const games = gamesData.games;
+  const [matchedGames, setMatchedGames] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  function handleNavSearch(e) {
+    const value = e.target.value.trim().toLowerCase();
+    setSearchValue(value);
+  }
+
+  useEffect(() => {
+    if (searchValue.length > 0) {
+      setMatchedGames(games.filter((game) => game.name.toLowerCase().includes(searchValue)).splice(0, 5));
+    } else {
+      setMatchedGames([]);
+    }
+  }, [searchValue]);
+
   return (
     <nav className="nav">
       <Link to="/" className="nav-logo">
         <img src={logo} /> MacVG
       </Link>
-      <input type="text" className="nav-search" placeholder="Search 500+ free online games" />
+      <div className="nav-search-container">
+        <div className="nav-search-bar">
+          <input
+            type="text"
+            className="nav-search"
+            placeholder="Search 500+ free online games"
+            value={searchValue}
+            onChange={handleNavSearch}
+          />
+          {searchValue.length > 0 && (
+            <img src={close} title="Clear search" className="nav-search-img" onClick={() => setSearchValue("")} />
+          )}
+        </div>
+        {matchedGames.length > 0 && (
+          <div className="nav-search-results">
+            {matchedGames.map((game) => (
+              <GameCard key={game.id} game={game} />
+            ))}
+          </div>
+        )}
+      </div>
       <Link to="/new" className="nav-link">
         New
       </Link>
